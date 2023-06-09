7167b1b28dc8cf75388697e166b37f15db57c9f8
mikhailsirenko <mikhailsirenko@gmail.com>
2023-06-09 16:26:38 +0200

    add docs and misc files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='unbreakable',
+    packages=find_packages(),
+    version='0.1.0',
+    description='',
+    author='Mikhail Sirenko, Bramka Arga Jafino, Brian Walsh',
+    license='CC BY 3.0 IGO',
+)
