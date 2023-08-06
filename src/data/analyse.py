657c9394e5d0b47f6091c85e2e85074435f36da9
Mikhail Sirenko <mikhailsirenko@gmail.com>
2023-08-06 18:26:18 +0200

    clean up

--- src/data/analyse.py
+++ src/data/analyse.py
@@ -190,15 +190,15 @@ def prepare_outcomes(results: tuple, add_policies: bool, add_uncertainties: bool
     outcomes['new_poverty_gap'] = outcomes['new_poverty_gap'] * 100
 
     # Calculate the percentage of new poor
-    # outcomes = outcomes.assign(n_new_poor_increase_pct=outcomes['n_new_poor'].div(
-    #     outcomes['total_population']).multiply(100))
+    outcomes = outcomes.assign(n_new_poor_increase_pct=outcomes['n_new_poor'].div(
+        outcomes['total_population']).multiply(100))
 
-    outcomes['pct_poor_before'] = outcomes['n_poor_initial'].div(
-        outcomes['total_population'])
-    outcomes['pct_poor_after'] = outcomes['n_new_poor'].add(
-        outcomes['n_poor_initial']).div(outcomes['total_population'])
-    outcomes['pct_poor_increase'] = outcomes['pct_poor_after'].sub(
-        outcomes['pct_poor_before'])
+    # outcomes['pct_poor_before'] = outcomes['n_poor_initial'].div(
+    #     outcomes['total_population'])
+    # outcomes['pct_poor_after'] = outcomes['n_new_poor'].add(
+    #     outcomes['n_poor_initial']).div(outcomes['total_population'])
+    # outcomes['pct_poor_increase'] = outcomes['pct_poor_after'].sub(
+    #     outcomes['pct_poor_before'])
 
     # Move years_in_poverty column to the end of the data frame
     outcomes = outcomes[[c for c in outcomes if c not in [
