fix wellbeing calculation

unbreakable/modules/integrator.py
@@ -199,10 +199,9 @@ def calculate_wellbeing(households: pd.DataFrame, consump_util: float, discount_
         # Check if any of the households has a negative consumption
         # It may happen if a household have very low savings or expenditure
         if (affected_households['c_t'] < 0).any():
-            # If so, then set the consumption to 0
-            affected_households.loc[affected_households['c_t'] < 0, 'c_t'] = 0
-            # * Previously it was set to 1
-            # affected_households.loc[affected_households['c_t'] < 1, 'c_t'] = 1
+            # If so, then set the consumption to 1
+            # We must have 1 to to avoid -inf in the wellbeing integration
+            affected_households.loc[affected_households['c_t'] < 0, 'c_t'] = 1
 
         # Consumption after the disaster should be lower than or equal to consumption before the disaster
         if (affected_households['c_t'] > affected_households['c_t_unaffected']).any():
@@ -236,8 +235,20 @@ def calculate_wellbeing(households: pd.DataFrame, consump_util: float, discount_
                                 < poverty_line_adjusted, 'weeks_pov'] += 1
 
         # Integrate well-being
-        affected_households['wellbeing'] = affected_households['c_t_unaffected']**(1 - consump_util)\
-            / (1-consump_util) * dt\
+        a = affected_households['c_t_unaffected']**(1 - consump_util)
+        b = (1 - consump_util) * dt
+        c = ((1 - ((affected_households['c_t_unaffected'] - affected_households['c_t']) / affected_households['c_t_unaffected'])
+              * np.e**(-affected_households['recovery_rate'] * _t))**(1 - consump_util) - 1)
+        d = np.e**(-discount_rate * _t)
+        e = a.div(b).mul(c).mul(d)
+        f = affected_households['c_t_unaffected']**(1 - consump_util)\
+            / (1 - consump_util) * dt\
+            * ((1 - ((affected_households['c_t_unaffected'] - affected_households['c_t']) / affected_households['c_t_unaffected'])
+                * np.e**(-affected_households['recovery_rate'] * _t))**(1 - consump_util) - 1)\
+            * np.e**(-discount_rate * _t)
+
+        affected_households['wellbeing'] += affected_households['c_t_unaffected']**(1 - consump_util)\
+            / (1 - consump_util) * dt\
             * ((1 - ((affected_households['c_t_unaffected'] - affected_households['c_t']) / affected_households['c_t_unaffected'])
                 * np.e**(-affected_households['recovery_rate'] * _t))**(1 - consump_util) - 1)\
             * np.e**(-discount_rate * _t)