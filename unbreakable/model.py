fix resilience/wellbeing calculation

unbreakable/model.py
@@ -74,6 +74,10 @@ def model(**params) -> dict:
     # Store outcomes in a dictionary, where the key is a district and value is a dictionary of outcomes
     outcomes = {}
 
+    # Calculate the wprime for the whole country
+    wprime = calculate_wprime(all_households, all_damage, districts, return_period,
+                              min_representative_households, random_seed, poverty_line, indigence_line, atol, consump_util)
+
     for district in districts:
         # Get total exposed asset stock and expected loss fraction for a specific district and return period
         tot_exposed_asset = get_tot_exposed_asset_stock(
@@ -108,7 +112,7 @@ def model(**params) -> dict:
                 f'../experiments/households/{district}_{random_seed}.csv')
 
         array_outcomes = np.array(list(get_outcomes(
-            households, tot_exposed_asset, expected_loss_frac, years_to_recover, consump_util).values()))
+            households, tot_exposed_asset, expected_loss_frac, years_to_recover, wprime).values()))
 
         # * To check whether we have different households affected in different runs
         # if district == 'Castries':

unbreakable/modules/households.py
@@ -5,13 +5,15 @@ It contains a set of functions that are used to calculate the impact of a disast
 import pandas as pd
 import numpy as np
 
+from unbreakable.data.reader import get_tot_exposed_asset_stock
+
 
 # def calculate_median_productivity(households: pd.DataFrame) -> pd.DataFrame:
-#     """Calculate the median productivity as a function of aeinc to k_house_ae, 
+#     """Calculate the median productivity as a function of aeinc to k_house_ae,
 
 #     where:
 #     - aeinc is the adult equivalent income (total)
-#     - k_house_ae is the domicile value divided by 
+#     - k_house_ae is the domicile value divided by
 #     the ratio of household expenditure to adult equivalent expenditure (per capita), calculated as:
 #       k_house_ae = domicile_value / (hhexp / aeexp)
 
@@ -451,3 +453,33 @@ def apply_policy(households: pd.DataFrame, my_policy: str) -> pd.DataFrame:
                    'aesav'] += households.loc[beneficiaries].eval('keff*v') * top_up / 100
 
     return households
+
+
+def calculate_wprime(all_households: pd.DataFrame,
+                     all_damage: pd.DataFrame,
+                     districts: list,
+                     return_period: int,
+                     min_representative_households: int,
+                     random_seed: int,
+                     poverty_line: float,
+                     indigence_line: float,
+                     atol: float,
+                     consump_util: float) -> pd.DataFrame:
+    '''Calculate wprime for the whole country.'''
+
+    households_adjusted = []
+    for district in districts:
+        tot_exposed_asset = get_tot_exposed_asset_stock(
+            all_damage, district, return_period)
+
+        households = all_households[all_households['district'] == district].copy(
+        )
+        households = (households.pipe(duplicate_households, min_representative_households, random_seed)
+                                .pipe(match_assets_and_expenditure, tot_exposed_asset, poverty_line, indigence_line, atol))
+        households_adjusted.append(households)
+    households_adjusted = pd.concat(households_adjusted)
+
+    # wprime is a factor that converts an abstract concept of wellbeing loss into consumption loss in monetary terms
+    wprime = (np.sum(
+        households_adjusted['aeexp'] * households_adjusted['popwgt']) / np.sum(households_adjusted['popwgt']))**(-consump_util)
+    return wprime

unbreakable/modules/integrator.py
@@ -201,6 +201,8 @@ def calculate_wellbeing(households: pd.DataFrame, consump_util: float, discount_
         if (affected_households['c_t'] < 0).any():
             # If so, then set the consumption to 1
             # We must have 1 to to avoid -inf in the wellbeing integration
+            # TODO: Experiment with 0.1 instead of 1
+            # This could be relevant if we do daily consumption/integration
             affected_households.loc[affected_households['c_t'] < 0, 'c_t'] = 1
 
         # Consumption after the disaster should be lower than or equal to consumption before the disaster
@@ -235,18 +237,6 @@ def calculate_wellbeing(households: pd.DataFrame, consump_util: float, discount_
                                 < poverty_line_adjusted, 'weeks_pov'] += 1
 
         # Integrate well-being
-        # a = affected_households['c_t_unaffected']**(1 - consump_util)
-        # b = (1 - consump_util) * dt
-        # c = ((1 - ((affected_households['c_t_unaffected'] - affected_households['c_t']) / affected_households['c_t_unaffected'])
-        #       * np.e**(-affected_households['recovery_rate'] * _t))**(1 - consump_util) - 1)
-        # d = np.e**(-discount_rate * _t)
-        # e = a.div(b).mul(c).mul(d)
-        # f = affected_households['c_t_unaffected']**(1 - consump_util)\
-        #     / (1 - consump_util) * dt\
-        #     * ((1 - ((affected_households['c_t_unaffected'] - affected_households['c_t']) / affected_households['c_t_unaffected'])
-        #         * np.e**(-affected_households['recovery_rate'] * _t))**(1 - consump_util) - 1)\
-        #     * np.e**(-discount_rate * _t)
-
         affected_households['wellbeing'] += affected_households['c_t_unaffected']**(1 - consump_util)\
             / (1 - consump_util) * dt\
             * ((1 - ((affected_households['c_t_unaffected'] - affected_households['c_t']) / affected_households['c_t_unaffected'])