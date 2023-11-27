69397814c6506c482d225681c736c08b0dbe26f5
Mikhail Sirenko <mikhailsirenko@gmail.com>
2023-11-27 16:14:28 +0100

    Clean up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='unbreakable',
     packages=find_packages(),
     version='0.3.0',
-    description='',
+    description='Unbreakable: Building the Resilience of the Poor in the Face of Natural Disasters a Simulation Model',
     author='Mikhail Sirenko and Bramka Arga Jafino',
     license='CC BY 3.0 IGO',
 )
