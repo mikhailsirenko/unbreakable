5e6a494d26f16a5ca6364f84cd1c08b3a88cf9dd
Mikhail Sirenko <mikhailsirenko@gmail.com>
2023-11-27 16:14:08 +0100

    Update version to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='unbreakable',
     packages=find_packages(),
-    version='0.2.2',
+    version='0.3.0',
     description='',
     author='Mikhail Sirenko and Bramka Arga Jafino',
     license='CC BY 3.0 IGO',
