cfc0878451407d8678b234cefd4d04970c86e40d
Mikhail Sirenko <mikhailsirenko@gmail.com>
2024-03-11 11:42:47 +0100

    Refactor function for better performance

--- unbreakable/modules/conflict.py
+++ unbreakable/modules/conflict.py
@@ -1,27 +1,23 @@
 import pandas as pd
 
 
-def affect_economy(conflict_impact: pd.DataFrame, avg_prod: float, inc_exp_growth: float) -> pd.DataFrame:
-    '''Calculate the effect of conflict on average productivity and income and expenditure growth.
+def calculate_conflict_impact(conflict: pd.DataFrame, avg_prod: float, inc_exp_growth: float) -> tuple:
+    '''Calculate the effect of a conflict on average productivity and income and expenditure growth by region.
 
     Args:
-        conflict_impact (pd.DataFrame): Conflict impact data.
-        avg_prod (float): Average productivity.
-        inc_exp_growth (float): Income and expenditure growth.
+        conflict (pd.DataFrame): Conflict data.
+        avg_prod (float): Base average productivity.
+        inc_exp_growth (float): Base income and expenditure growth.
 
     Returns:
-        pd.DataFrame: Affected economy.
+        pd.DataFrame: Affected average productivity and income and expenditure growth.
     '''
-    # Make a copy of the conflict impact data
-    df = conflict_impact.copy()
-
     # Decrease the average productivity by Avg. prod. decrease %
-    df['avg_prod'] = avg_prod - \
-        df['Avg. prod. decrease'] * avg_prod
+    conflict['avg_prod'] = avg_prod - \
+        conflict['Avg. prod. decrease'] * avg_prod
 
     # Decrease the income and expenditure growth by Inc. exp. growth decrease %
-    df['inc_exp_growth'] = inc_exp_growth - \
-        df['Inc. and exp. growth decrease'] * inc_exp_growth
+    conflict['inc_exp_growth'] = inc_exp_growth - \
+        conflict['Inc. and exp. growth decrease'] * inc_exp_growth
 
-    # Return affected economy
-    return df
+    return conflict.set_index('region')['avg_prod'], conflict.set_index('region')['inc_exp_growth']
