0403fc7768acf4cee6e16d3571be77efab9d21c3
Mikhail Sirenko <mikhailsirenko@gmail.com>
2024-03-11 11:43:22 +0100

    Minor changes and refactoring

--- unbreakable/modules/households.py
+++ unbreakable/modules/households.py
@@ -2,7 +2,24 @@ import pandas as pd
 import numpy as np
 
 
-def estimate_impact(households: pd.DataFrame, region_pml: float, pov_bias: float, calc_exposure_params: dict) -> pd.DataFrame:
+def calculate_exposure(households: pd.DataFrame, region_pml: float, pov_bias: float, calc_exposure_params: dict) -> pd.DataFrame:
+    '''Calculate the exposure of each household to the disaster.
+
+    Households have either have an equal change of being affected by the disaster or a change based on their poverty bias. `fa` stands for `fraction affected` and represents the impact of the disaster on each household. 
+
+    Args:
+        households (pd.DataFrame): Households.
+        region_pml (float): Region PML.
+        pov_bias (float): Poverty bias.
+        calc_exposure_params (dict): Parameters for calculating exposure.
+
+    Returns:
+        pd.DataFrame: Households with a new `fa` column representing the impact of the disaster.
+
+    Raises:
+        ValueError: If only uniform distribution is supported yet.
+    '''
+
     # Random value for poverty bias
     if pov_bias == 'random':
         if calc_exposure_params['distr'] == 'uniform':
@@ -10,37 +27,39 @@ def estimate_impact(households: pd.DataFrame, region_pml: float, pov_bias: float
             low = calc_exposure_params['low']
             # default 1.5
             high = calc_exposure_params['high']
-            povbias = np.random.uniform(low, high)
+            poverty_bias = np.random.uniform(low, high)
         else:
             raise ValueError("Only uniform distribution is supported yet.")
     else:
-        povbias = pov_bias
+        poverty_bias = pov_bias
 
     # Set poverty bias to 1 for all households
     households['pov_bias'] = 1
 
     # Set poverty bias to povbias for poor households
-    households.loc[households['is_poor'] == True, 'pov_bias'] = povbias
+    households.loc[households['is_poor'] == True, 'pov_bias'] = poverty_bias
 
-    # !: If keff is just k_house, why not just use k_house?
+    # Keff is the effective capital stock of the household
+    # Physical assets such as land, housing, and durable goods
+    # Currently, only the dwelling value k_house is considered
     households['keff'] = households['k_house'].copy()
 
-    # Calculate the exposure of each household
+    # How much each household is affected by the disaster
     normalization_factor = households[['keff', 'v', 'pov_bias', 'wgt']].prod(
         axis=1).sum()
 
-    # Distribute the impact of the disaster across households
+    # Distribute the impact of the disaster given region's PML across households
     households['fa'] = (region_pml / normalization_factor) * \
-        households[['pov_bias']]
+        households['pov_bias']  # `Poverty bias` allows to re-distribute impact between poor and non-poor households
 
     return households
 
 
 def identify_affected(households: pd.DataFrame, region_pml: float, ident_affected_params: dict, random_seed: int) -> pd.DataFrame:
-    '''Determines affected households.
+    '''Determine affected households.
 
     We assume that all households have the same chance of being affected, 
-    but based on `fa` value calculated in `estimate_impact`.
+    but based on `fa` value calculated in `calculate_exposure`.
 
     Args:
         households (pd.DataFrame): Households.
@@ -54,9 +73,10 @@ def identify_affected(households: pd.DataFrame, region_pml: float, ident_affecte
         ValueError: If no mask was found.
     '''
     # BUG: For whatever reason fixing random seed in the model.py doesn't work here
-    np.random.seed(random_seed)
+    if random_seed is not None:
+        np.random.seed(random_seed)
 
-    # Allow for a relatively small error
+    # Allow for a relatively small error between the total asset loss and the PML
     delta = region_pml * ident_affected_params['delta_pct']  # default 0.025
 
     # Check if total asset is less than PML
@@ -69,7 +89,7 @@ def identify_affected(households: pd.DataFrame, region_pml: float, ident_affecte
     high = ident_affected_params['high']  # default 1
 
     # Generate multiple boolean masks at once
-    num_masks = ident_affected_params['num_masks']  # default 2000
+    num_masks = ident_affected_params['num_masks']  # default 10,000
     masks = np.random.uniform(
         low, high, (num_masks, households.shape[0])) <= households['fa'].values
 
@@ -113,12 +133,16 @@ def identify_affected(households: pd.DataFrame, region_pml: float, ident_affecte
         raise ValueError(
             f'Total asset loss ({tot_asset_loss}) is not within the desired range.')
 
+    # num_affected = households['is_affected'].sum()
+    # print(f'Number of affected households: {num_affected}')
+    # print(households[households['is_affected']].index)
+
     return households
 
 
-def calculate_welfare(country_households: pd.DataFrame, cons_util: float) -> float:
-    # TODO: Make sure that the function name and what is returns is correct
-    # TODO: Add a docstring
-    welfare = (np.sum(country_households['exp'] * country_households['wgt']) / np.sum(
-        country_households['wgt'])) ** (-cons_util)
+def calculate_welfare(households: pd.DataFrame, cons_util: float) -> float:
+    '''Calculate the welfare of all households in a country based on their expenditures and a given consumption utility.'''
+    weighted_average_expenditure = np.sum(households['exp'] * households['wgt']) / np.sum(
+        households['wgt'])
+    welfare = weighted_average_expenditure ** (-cons_util)
     return welfare
