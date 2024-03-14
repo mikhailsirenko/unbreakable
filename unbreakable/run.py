1cab2223b5437f3a1cc234a2e22e065e4e1cda1b
Mikhail Sirenko <mikhailsirenko@gmail.com>
2024-03-14 15:44:21 +0100

    Update config and experimental setup in run.py

--- unbreakable/run.py
+++ unbreakable/run.py
@@ -7,17 +7,18 @@ if __name__ == "__main__":
     try:
         country = 'Nigeria'
         return_period = 100
+        conflict = True
 
-        config = load_config(country, return_period)
+        config = load_config(country, return_period, conflict)
         model = setup_model(config, replicator=False)
 
         experimental_setup = {
             'country': country,
             'return_period': return_period,
             'model': model,
-            'n_scenarios': 12,
+            'n_scenarios': 2,
             'n_policies': 0,
-            'multiprocessing': True,
+            'multiprocessing': False,
             'n_processes': 12
         }
 
